4dc18a9d29f5d1872e0d1285bcd1d516d203a67d
Kou Vang <kouvang322@hotmail.com>
2024-07-17 15:05:42 -0500

    changed dist location

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/Task_Tracker/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/task-tracking/index.html'));
 });
 
 // dbFunctions.createTable();
